d50d285af88d70dda6c8b9eb4c51a645c4e88b61
J0ELh <joelhempel35@gmail.com>
2023-03-06 22:08:26 -0800

    fixed AI initial timer

--- Assets/Scripts/Controllers/AIController.cs
+++ Assets/Scripts/Controllers/AIController.cs
@@ -10,14 +10,17 @@ public class AIController : InputController
 {
     private float movement = -1f;
     private float switchRate = 2f;
-    private float nextSwitch = 0f;
+    private static float nextSwitch = 0f;
   //  System.Timers.Timer T = new System.Timers.Timer();
   //  T.Interval = 2000;
    // T.Elapsed += delegate;
     
     
 
-
+    void Start()
+    {
+        nextSwitch = 0f;
+    }
 
     public override bool RetrieveJumpInput() {
         
