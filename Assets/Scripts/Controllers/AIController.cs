efaaac073dc6465bb7e4aa6aca7cb0b59c2ed252
J0ELh <joelhempel35@gmail.com>
2023-03-06 16:10:15 -0800

    meeting commit

--- Assets/Scripts/Controllers/AIController.cs
+++ Assets/Scripts/Controllers/AIController.cs
@@ -4,13 +4,35 @@ using UnityEngine;
 
 [CreateAssetMenu(fileName = "AIController", menuName = "InputController/AIController")]
 
+
+
 public class AIController : InputController
 {
+    private float movement = -1f;
+    private float switchRate = 2f;
+    private float nextSwitch = 0f;
+  //  System.Timers.Timer T = new System.Timers.Timer();
+  //  T.Interval = 2000;
+   // T.Elapsed += delegate;
+    
+    
+
+
+
     public override bool RetrieveJumpInput() {
+        
         return true;
     }
 
     public override float RetrieveMoveInput() {
-        return 1f;
+
+        Debug.Log("test " + Time.time + "    " + nextSwitch);
+        
+        if (Time.time > nextSwitch) { 
+            nextSwitch = Time.time + switchRate;
+            movement =  movement * (-1f);
+        }
+
+        return movement;
     }
 }
